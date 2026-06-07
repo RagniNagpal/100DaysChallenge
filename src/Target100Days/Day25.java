38fdf865579134df8fe67679cfad4906c6cb3a69
RagniNagpal <ragninagpal@gmail.com>
2026-06-07 11:32:49 +0530

    Day25

--- src/Target100Days/Day25.java
+++ src/Target100Days/Day25.java
@@ -194,6 +194,35 @@ public class Day25 {
             nums1[k--] = nums2[j--];
         }
     }
+
+    //two sum
+    // tc o(n^2) sc O(1)
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i] + nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    //tc , sc O(n)
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+
     public static void main(String[] args) {
 
     }
