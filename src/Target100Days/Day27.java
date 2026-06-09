99d084f4d970500e801d2f0399028a01a4c97756
RagniNagpal <ragninagpal@gmail.com>
2026-06-09 16:30:11 +0530

    Day27

--- src/Target100Days/Day27.java
+++ src/Target100Days/Day27.java
@@ -14,6 +14,41 @@ public class Day27 {
         return mirror(t1.left,t2.right) && mirror(t2.left,t1.right);
 
     }
+
+    //floor
+    //tc O(h) sc O(1)
+    public int findMaxFork(Node root, int k) {
+        // code here.
+        int floor=-1;
+        while(root != null){
+            if(root.data==k) return root.data;
+            else if(root.data < k){
+                floor=root.data;
+                root=root.right;
+            }else{
+                root=root.left;
+            }
+        }
+        return floor;
+    }
+
+    //ceil
+    //tc O(h) sc O(1)
+    int findCeil(Node root, int x) {
+        // code here
+        int ceil=-1;
+        while(root != null){
+            if(root.data==x) return root.data;
+            else if(root.data > x){
+                ceil=root.data;
+                root=root.left;
+            }else{
+                root=root.right;
+            }
+        }
+        return ceil;
+    }
+
     public static void main(String[] args) {
 
     }
