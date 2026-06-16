0ca79e0051aac28e10c05ba5f7e1307219bc4063
RagniNagpal <ragninagpal@gmail.com>
2026-06-16 11:41:07 +0530

    Day34

--- src/Target100Days/Day34.java
+++ src/Target100Days/Day34.java
@@ -1,4 +1,5 @@
 package Target100Days;
+import java.util.*;
 
 public class Day34 {
     //Search in BST
@@ -28,8 +29,62 @@ public class Day34 {
     }
 
     //Combination sum
-    //
+    //✅ Time Complexity: O(2^target) (exponential)
+    //✅ Space Complexity: O(target) (recursion stack + current path)
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ds=new ArrayList<>();
+        int n=candidates.length;
+        List<Integer> res = new ArrayList<>();
+        solve(0,candidates,new ArrayList<>(),target,ds);
+        return ds;
 
+    }
+    public static void solve(int idx,int[] candidates,List<Integer> res,int target,List<List<Integer>> ds){
+        if(target == 0){
+            ds.add(new ArrayList<>(res));
+            return;
+        }
+        if(idx == candidates.length) {
+            return;
+        }
+        if(candidates[idx] <= target){
+            res.add(candidates[idx]);
+            solve(idx,candidates,res,target-candidates[idx],ds);
+            res.remove(res.size()-1);}
+        solve(idx+1,candidates,res,target,ds);
+    }
+
+    //combination sum 2
+    //Time Complexity: O(2^n)
+    //✅ Space Complexity: O(n)
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result=new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
+        int n=candidates.length;
+        solve(0,candidates,target,ans,result);
+        return result;
+    }
+    public static void solve(int idx,int[] candidates,int target,List<Integer> ans,List<List<Integer>> result){
+        if(target==0){
+            result.add(new ArrayList<>(ans));
+            return;
+        }
+        if(idx == candidates.length) {
+            return;
+        }
+        for(int i=idx;i<candidates.length;i++){
+            if(candidates[i] > target) {
+                break;
+            }
+            if(i > idx && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            ans.add(candidates[i]);
+            solve(i+1,candidates,target-candidates[i],ans,result);
+            ans.remove(ans.size()-1);
+        }
+    }
     public static void main(String[] args) {
 
     }
