6a5b32d9a36ef405c5adff6f08816d1afcd2fe77
RagniNagpal <ragninagpal@gmail.com>
2026-06-17 14:38:47 +0530

    Day35

--- src/Target100Days/Day35.java
+++ src/Target100Days/Day35.java
@@ -88,6 +88,18 @@ public class Day35 {
         return -1;
     }
 
+    //Validate Binary Search Tree
+    //tc O(n) Sc o(h)
+    public boolean isValidBST(TreeNode root) {
+        return solve(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    public static boolean solve(TreeNode root,long min,long max){
+        if(root==null) return true;
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return solve(root.left,min,root.val) && solve(root.right,root.val,max);
+    }
     public static void main(String[] args) {
 
     }
