5fb78de3eb31edd661e6cffff3c63c6e351607cd
RagniNagpal <ragninagpal@gmail.com>
2026-06-19 14:30:16 +0530

    Day37

--- src/Target100Days/Day37.java
+++ src/Target100Days/Day37.java
@@ -125,6 +125,48 @@ public class Day37 {
         return root;
 
     }
+
+    // implement minstack
+    //tc O(!) sc O(n)
+    class MinStack {
+        private Stack<Integer> stack;
+        private Stack<Integer> minstack;
+
+        public MinStack() {
+            stack=new Stack<>();
+            minstack=new Stack<>();
+        }
+
+        public void push(int value) {
+            stack.push(value);
+            if(minstack.isEmpty()){
+                minstack.push(value);
+            }
+            else{minstack.push(Math.min(value,minstack.peek()));}
+        }
+
+        public void pop() {
+            stack.pop();
+            minstack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minstack.peek();
+        }
+    }
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.push(value);
+     * obj.pop();
+     * int param_3 = obj.top();
+     * int param_4 = obj.getMin();
+     */
     public static void main(String[] args) {
 
     }
