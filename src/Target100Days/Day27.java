bf64300e8d8c7c4ae0fcc0dba5147e7d5edd2be5
RagniNagpal <ragninagpal@gmail.com>
2026-06-20 10:36:23 +0530

    Day27

--- src/Target100Days/Day27.java
+++ src/Target100Days/Day27.java
@@ -1,5 +1,6 @@
 package Target100Days;
 
+
 public class Day27 {
     //Symmetric tree
     //TC O(n) Sc O(H)
@@ -12,7 +13,6 @@ public class Day27 {
         if(t1==null || t2==null) return false;
         if(t1.val!=t2.val) return false;
         return mirror(t1.left,t2.right) && mirror(t2.left,t1.right);
-
     }
 
     //floor
@@ -49,7 +49,77 @@ public class Day27 {
         return ceil;
     }
 
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
 
+        Day27 obj = new Day27();
+
+        // ================= Symmetric Tree =================
+
+    /*
+              1
+            /   \
+           2     2
+          / \   / \
+         3   4 4   3
+    */
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println("Is Symmetric: "
+                + obj.isSymmetric(root));
+
+        // ================= BST for Floor & Ceil =================
+
+    /*
+                10
+              /    \
+             5      15
+            / \    /  \
+           2   7  12  20
+    */
+
+        Node bst = new Node(10);
+
+        bst.left = new Node(5);
+        bst.right = new Node(15);
+
+        bst.left.left = new Node(2);
+        bst.left.right = new Node(7);
+
+        bst.right.left = new Node(12);
+        bst.right.right = new Node(20);
+
+        int k = 13;
+
+        System.out.println("Floor of " + k + " = "
+                + obj.findMaxFork(bst, k));
+
+        System.out.println("Ceil of " + k + " = "
+                + obj.findCeil(bst, k));
     }
 }
