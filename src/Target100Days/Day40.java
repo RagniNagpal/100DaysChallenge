1f66b58d32f6104de21ce20f236cf4ce42ba6f1f
RagniNagpal <ragninagpal@gmail.com>
2026-06-22 17:48:18 +0530

    Day40

---
+++ src/Target100Days/Day40.java
@@ -0,0 +1,23 @@
+package Target100Days;
+
+public class Day40 {
+    int count=0;
+    int ans=-1;
+    public int kthSmallest(TreeNode root, int k) {
+        kth(root,k);
+        return ans;
+    }
+    public void kth(TreeNode root,int k){
+        if(root==null) return;
+        kth(root.left,k);
+        count++;
+        if(count==k){
+            ans=root.val;
+            return;
+        }
+        kth(root.right,k);
+    }
+    public static void main(String[] args) {
+
+    }
+}
