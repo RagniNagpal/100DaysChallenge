2f765c8a14ad706c5d1ad42a908ab434e0b3ac17
RagniNagpal <ragninagpal@gmail.com>
2026-07-10 20:47:01 +0530

    Day56

--- src/Target100Days/Day56.java
+++ src/Target100Days/Day56.java
@@ -31,7 +31,7 @@ public class Day56 {
         return next;
     }
     public static boolean palin(ListNode head){
-        if(head==null || head.next==null) return head;
+//        if(head==null || head.next==null) return head;
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
@@ -48,8 +48,96 @@ public class Day56 {
         return true;
     }
 
-    //
-    public static void main(String[] args) {
+    //Recursion
 
+    //sabhki O(n) tc sc
+    //print name
+    public static int print(int n){
+        if(n==0){
+            return 0;
+        }
+        System.out.println("Ragni");
+        return print(n-1);
+    }
+
+    //print n to 1
+    public static int range1(int n){
+        if(n==0){
+            return 0;
+        }
+        System.out.println(n);
+        return range1(n-1);
+    }
+    //print 1 to n
+    public static void range2(int n){
+        if(n==0){
+            return;
+        }
+        range2(n-1);
+        System.out.println(n);
+    }
+
+    //sum of first n
+    public static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n + sum(n-1);
+    }
+
+    //factorial
+    public static int fact(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+
+    //reverse array
+    public static void reverse(int[] arr){
+        solve(0,arr.length-1,arr);
+    }
+    public static void solve(int start,int end,int[] arr){
+        if(start >=end){
+            return;
+        }
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        solve(start+1,end-1,arr);
+    }
+
+//    check palindrome
+    public static boolean palin(String str){
+        return solve(0, str.length()-1, str);
+    }
+    public static boolean solve(int start,int end,String str){
+        if(start>=end){
+            return true;
+        }
+        if(str.charAt(start)!=str.charAt(end)){
+            return false;
+        }
+        return solve(start+1,end-1,str);
+    }
+
+
+    public static void main(String[] args) {
+        int n=5;
+        print(n);
+        range1(n);
+        range2(n);
+        System.out.println(sum(n));
+        System.out.println(fact(n));
+        int[] arr = {1,2,3,4,5};
+
+        reverse(arr);
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        String str = "madam";
+        System.out.println(palin(str));
     }
 }
