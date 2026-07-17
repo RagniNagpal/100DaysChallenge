6d6955aa58725d014522158d3447752183e698e9
RagniNagpal <ragninagpal@gmail.com>
2026-07-17 21:59:07 +0530

    DP

--- src/DP/Day5.java
+++ src/DP/Day5.java
@@ -1,29 +1,12 @@
 package DP;
 import java.util.*;
 
-public class Day4 {
-    //LCS
-    public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp=new int[text1.length()+1][text2.length()+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
-        }
-        return solve(0, 0, text1, text2, dp);
+public class Day5 {
+    //min insertions steps to make a string pakindrome
+    public int minInsertions(String s) {
+        int ans=longestPalindromeSubseq(s);
+        return s.length()-ans;
     }
-    public static int solve(int i, int j, String text1, String text2, int[][] dp){
-        if(i==text1.length() || j==text2.length()) return 0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int pick = 0;
-        if(text1.charAt(i) == text2.charAt(j)){
-            return dp[i][j] =1 + solve(i+1, j+1, text1, text2, dp);}
-        else{
-            return dp[i][j] = Math.max(solve(i+1, j, text1, text2, dp),solve(i, j+1, text1, text2, dp));
-        }
-    }
-
-    //longest palindromic subsequence
     public int longestPalindromeSubseq(String s) {
         String text2="";
         for(int i=s.length()-1;i>=0;i--){
@@ -51,7 +34,8 @@ public class Day4 {
         }
     }
 
-    //
+    //edit distance
+
     public static void main(String[] args) {
 
     }
