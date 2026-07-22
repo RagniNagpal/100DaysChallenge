495dded871af9d3ca35513e40401f77453553eca
RagniNagpal <ragninagpal@gmail.com>
2026-07-22 21:18:01 +0530

    Heaps

--- Heaps/MedianfromDataStream.java
+++ Heaps/MedianfromDataStream.java
@@ -1,5 +1,37 @@
 package Heaps;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class MedianfromDataStream {
   
+    PriorityQueue<Integer> left;  // Max-Heap
+    PriorityQueue<Integer> right; // Min-Heap
+
+    public MedianFinder() {
+        left = new PriorityQueue<>(Collections.reverseOrder());
+        right = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        if (left.isEmpty() || num <= left.peek()) {
+            left.offer(num);
+        } else {
+            right.offer(num);
+        }
+
+        // Balance
+        if (left.size() > right.size() + 1) {
+            right.offer(left.poll());
+        } else if (right.size() > left.size()) {
+            left.offer(right.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (left.size() == right.size()) {
+            return (left.peek() + right.peek()) / 2.0;
+        }
+        return left.peek();
+    }
 }
--- Heaps/MergeKSortedList.java
+++ Heaps/MergeKSortedList.java
@@ -1,5 +1,35 @@
 package Heaps;
 
+import java.util.PriorityQueue;
+
 public class MergeKSortedList {
-  
+  public ListNode mergeKLists(ListNode[] lists) {
+
+        // Min-Heap based on node value
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Step 1: add head of each list
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        // Step 2: process heap
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            curr.next = minNode;
+            curr = curr.next;
+
+            if (minNode.next != null) {
+                pq.add(minNode.next);
+            }
+        }
+
+        return dummy.next;
+    }
 }
