0cc051912367d963fda6511b124d09112f990cb1
RagniNagpal <ragninagpal@gmail.com>
2026-07-30 21:46:14 +0530

    Revision

---
+++ ReRevise/MergeSort.java
@@ -0,0 +1,68 @@
+package ReRevise;
+
+public class MergeSort {
+
+    public static void mergeSort(int[] arr, int l, int r) {
+
+        if (l >= r)
+            return;
+
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int mid, int r) {
+
+        int[] temp = new int[r - l + 1];
+
+        int i = l;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= r) {
+
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= r) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int x = 0; x < temp.length; x++) {
+            arr[l + x] = temp[x];
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {8, 3, 5, 4, 7, 6, 1, 2};
+
+        System.out.println("Before Sorting:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("After Sorting:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ ReRevise/reverse.java
@@ -0,0 +1,62 @@
+package ReRevise;
+
+public class reverse {
+  class LeftRotate {
+
+    static void rotateLeft(int[] arr, int k) {
+
+        int n = arr.length;
+
+        k = k % n;
+
+        reverse(arr, 0, k - 1);
+
+        reverse(arr, k, n - 1);
+
+        reverse(arr, 0, n - 1);
+    }
+
+    static void reverse(int[] arr, int start, int end) {
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
+
+//
+class Solution {
+
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+
+        reverse(nums, 0, k - 1);
+
+        reverse(nums, k, n - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+
+        while (start < end) {
+
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
+}
